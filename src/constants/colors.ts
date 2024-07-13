5363cc61307907ebec1995ad8f8b75ae8d1327d5
Cristian Bonilla <cristiancamilo10_95@outlook.com>
2024-07-12 22:36:36 -0500

    :pencil2: Type of default colors added

---
+++ src/constants/colors.ts
@@ -0,0 +1,13 @@
+import { ColorValue } from 'react-native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+
+export const COLORS = Colors as Record<
+  'primary' |
+  'white' |
+  'lighter' |
+  'light' |
+  'dark' |
+  'darker' |
+  'black',
+  ColorValue
+>;
